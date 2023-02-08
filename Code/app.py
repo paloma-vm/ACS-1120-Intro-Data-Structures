cf67fc7e602642a7f62e2f1e7e193398dc675172
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-02-07 23:57:48 -0700

    app.py generates a sentence in browser

--- Code/app.py
+++ Code/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 import random
 import sys
-from sentence import read_words
+from sentence import make_sentence
 import cleanup
 
 
@@ -23,27 +23,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    filename = '/usr/share/dict/words'
+    filename = 'test-text.txt'
     number_of_words = 6
     # random_words = []
-    generated_sentence = read_words(filename, number_of_words)
-    print(generated_sentence)
+    generated_sentence = make_sentence(filename, number_of_words)
+    print('____________________________________')
+    # print(generated_sentence)
     return f"<p>{generated_sentence}</p>"
-    
-    # return read_words('/usr/share/dict/words', 6)
-    # context = {
-    #     'filename': filename,
-    #     'number_of_words': number_of_words,
-    #     'random_words': random_words,
-        # 'generated_sentence': generated_sentence
-    # }
-
-    # return render_template('index.html')
-
-    # return render_template('index.html', **context)
-
-    # return "<p>TODO: Return a word here!</p>"
-    # return generated_sentence
+    # return f"<p>m</p>"
 
 
 if __name__ == "__main__":
@@ -51,7 +38,5 @@ if __name__ == "__main__":
        To learn more about Flask's DEBUG mode, visit
        https://flask.palletsprojects.com/en/2.0.x/server/#in-code"""
     app.run(debug=True)
-    # filename = str(sys.argv[1])
-    # words_list = cleanup.cleanup(filename)
-    # print(words_list)
+
     
--- Code/sentence.py
+++ Code/sentence.py
@@ -1,12 +1,13 @@
 import random
 import sys
+import string
 
 # class Sentence():
 # sentence = ''
 # filename = 'short-dictionary.txt'
-filename = '/usr/share/dict/words'
+# filename = '/usr/share/dict/words'
 # number_of_words = int(sys.argv[1])
-number_of_words = 6
+# number_of_words = 6
 
 # random_words = []
 # read in the words file
@@ -34,8 +35,40 @@ def read_words(filename, number_of_words):
     sentence = print(str(random_words[-1]) + ".") #puts the period in the correct place at end of sentence
 
     return sentence
+
+def make_sentence(filename, number_of_words):
+    """
+    A function to read the text file of words, 
+    select a random set of words and store in a data type, 
+    put the words into a "sentence", and output the sentence
+    """
+    words_list = []
+    infile = open(filename, "r")
+
+    lines = infile.readlines() # reads whole file
+    for line in lines:
+        line = line.strip()
+        line = line.lower()
+        line = line.translate(line.maketrans("", "", string.punctuation))
+
+        words_in_line = line.split(" ")
+
+        words_list.extend(words_in_line)
+
+    infile.close
+
+    random_words = random.sample(words_list, k=number_of_words)
+    print("********************")
+    print(random_words)
+    print("----------------")
+
+    sentence = " ".join(random_words) + "."
+        
+    print('########################')
+    print(sentence)
+    return sentence
+    
     
 if __name__ == '__main__':
-    read_words('/usr/share/dict/words', 6)
-
+    make_sentence('/usr/share/dict/words', 6)
     
