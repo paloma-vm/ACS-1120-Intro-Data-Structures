a826cd6c16f97b8cd07e5b7678f1bd5698a74682
Paloma Valdez-Marsh <palomavaldez-marsh@Palomas-MacBook-Air.local>
2023-03-01 02:21:47 -0700

    Update app.py
    Changed app.py to use Markov Chain to generate sentence

--- Code/app.py
+++ Code/app.py
@@ -4,6 +4,9 @@ import random
 import sys
 from sentence import make_sentence
 import cleanup
+import pprint
+from file_to_words import file_to_words
+from nth_markov import MarkovChain
 
 
 
@@ -13,24 +16,34 @@ app = Flask(__name__)
 # Any code placed here will run only once, when the server starts.
 
 
-# words_list = cleanup.cleanup(filename)
 
 
 
 
+# Before Markov
+# -------------------------------------------------------------
+# @app.route("/")
+# def home():
+#     """Route that returns a web page containing the generated text."""
+#     filename = 'test-text.txt'
+#     number_of_words = 6
+#     # random_words = []
+#     generated_sentence = make_sentence(filename, number_of_words)
+#     print('____________________________________')
+#     # print(generated_sentence)
+#     return f"<p>{generated_sentence}</p>"
+#     # return f"<p>m</p>"
 
+
+# Using Markov
 # -------------------------------------------------------------
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    filename = 'test-text.txt'
-    number_of_words = 6
-    # random_words = []
-    generated_sentence = make_sentence(filename, number_of_words)
-    print('____________________________________')
-    # print(generated_sentence)
-    return f"<p>{generated_sentence}</p>"
-    # return f"<p>m</p>"
+    corpus = file_to_words('text_files/peas.txt')
+    markov_chain = MarkovChain(corpus=corpus, order=2)
+    generated_sentence = markov_chain.random_walk(9)
+    return generated_sentence
 
 
 if __name__ == "__main__":
